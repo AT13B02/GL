d7f420d7bf7f7fab494b2d35424ab7f5fb2af85f
KabutomoriKenji <kab2168@gmail.com>
2014-12-01 16:37:27 +0900

    キャラクターカメラの作成

--- framework/sources/interface/character/camera/character_camera.h
+++ framework/sources/interface/character/camera/character_camera.h
@@ -1,31 +1,31 @@
 //*****************************************************************************
 //
-// インターフェースマネージャークラス
+// キャラクターカメラクラス
 //
 // Author		: Kenji Kabutomori
 //
 //*****************************************************************************
 
 //*****************************************************************************
-// 二重定義防止
+// 多重定義防止
 //*****************************************************************************
 #pragma once
 
-#ifndef _INTERFACE_MANAGER_H_
-#define _INTERFACE_MANAGER_H_
+#ifndef _CHARACTER_CAMERA_H_
+#define _CHARACTER_CAMERA_H_
 
 //*****************************************************************************
 // インクルード
 //*****************************************************************************
+// basic
 #include "basic/basic.h"
-#include "basic/main_window.h"
+
+//*****************************************************************************
+// ライブラリのリンク
+//*****************************************************************************
 
 //*****************************************************************************
 // マクロ定義
-//*****************************************************************************
-
-//*****************************************************************************
-// 定数定義
 //*****************************************************************************
 
 //*****************************************************************************
@@ -35,65 +35,38 @@
 //*****************************************************************************
 // クラスの前方参照
 //*****************************************************************************
-class CInputManager;
-class CSoundManager;
-class CGraphicManager;
-class CNetworkManager;
+class CInterfaceManager;
 
 //*****************************************************************************
 // クラス定義
 //*****************************************************************************
-class CInterfaceManager : public CBasic
+class CCharacterCamera : public CBasic
 {
 public:
 	// コンストラクタ
-	CInterfaceManager(WINDOW_DATA* window_data);
+	CCharacterCamera(CInterfaceManager* interface_manager);
 
 	// デストラクタ
-	~CInterfaceManager(void);
+	virtual ~CCharacterCamera(void);
 
-	// 初期化処理
+	// 初期化
 	bool Init(void);
 
-	// 更新処理
+	// 更新
 	void Update(void);
 
-	// 描画処理
+	// 描画
 	void Draw(void);
 
-	// 終了処理
+	// 終了
 	void Uninit(void);
 
-	// ロード開始
-	void BeginLoad(void);
-
-	// ロード終了
-	void EndLoad(void);
-
-	// ウィンドウデータの取得
-	WINDOW_DATA* window_data(void){return window_data_;}
-
-	// インプットマネージャーの取得
-	CInputManager* input_manager(void){return input_manager_;}
-
-	// サウンドマネージャーの取得
-	CSoundManager* sound_manager(void){return sound_manager_;}
-
-	// グラフィックマネージャーの取得
-	CGraphicManager* graphic_manager(void){return graphic_manager_;}
-
-	// ネットワークマネージャーの取得
-	CNetworkManager* network_manager(void){return network_manager_;}
 protected:
 
 private:
-	WINDOW_DATA* window_data_;
-	CInputManager* input_manager_;
-	CSoundManager* sound_manager_;
-	CGraphicManager* graphic_manager_;
-	CNetworkManager* network_manager_;
+	CInterfaceManager* interface_manager_;
 };
 
-#endif // _INTERFACE_MANAGER_H_
+#endif	// _CHARACTER_CAMERA_H_
 
 //---------------------------------- EOF --------------------------------------
