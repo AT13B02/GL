16f244adc145ec44f02f3bde6e4e85f619170202
Tomohiro kouno <kounotomohiro_hal@yahoo.co.jp>
2014-12-03 15:25:31 +0900

    何故か更新されてなかったものを追加。マージはまだです。

---
+++ framework/sources/interface/graphic/object/object_2d/element/rectangle_2d.cpp
@@ -0,0 +1,305 @@
+//*****************************************************************************
+//
+// 矩形2Dクラス [rectangle_2d.cpp]
+//
+// Author		: KENJI KABUTOMORI
+// Date			: 2014/04/21(Mon)
+// Version		: 1.00
+// Update Date	: 2014/10/11(Thu)
+//
+//*****************************************************************************
+
+//*****************************************************************************
+// インクルード
+//*****************************************************************************
+#include "basic/application.h"
+
+// graphic
+#include "interface/graphic/vertex/vertex_2d.h"
+#include "interface/graphic/object/object_2d/element/rectangle_2d.h"
+#include "interface/graphic/object/object_2d/object_2d_data.h"
+#include "interface/graphic/texture/texture_manager.h"
+#include "interface/graphic/texture/texture.h"
+
+// common
+#include "common/common.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+
+//*****************************************************************************
+// グローバル変数
+//*****************************************************************************
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CRectangle2D::CRectangle2D(CDeviceHolder* device_holder) : CObject2D(device_holder,OBJECT_2D_TYPE_RECTANGLE)
+{
+	left_   = 0.0f;
+	right_  = 1.0f;
+	top_    = 0.0f;
+	bottom_ = 1.0f;
+
+	point_ = POINT_CENTER;
+
+	size_ = VECTOR2(0.0f,0.0f);
+
+
+	for(int i = 0;i < VERTEX_MAX;i++)
+	{
+		color_[i] = COLOR4F(1.0f,1.0f,1.0f,1.0f);
+	}
+
+	vertex_2d_ = CVertex2D::Create(device_holder,4);
+}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CRectangle2D::~CRectangle2D(void)
+{
+}
+
+//=============================================================================
+// 初期化
+//=============================================================================
+bool CRectangle2D::Init(void)
+{
+	return true;
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CRectangle2D::Draw(CObject2DData* object_2d_data)
+{
+	MATRIX4x4 matrix;
+	CTextureManager* texture_manager = object_2d_data->texture_manager();
+	CTexture* texture = texture_manager->Get(object_2d_data->texture_name().c_str());
+	CRenderstate* renderstate = object_2d_data->renderstate();
+
+	//matrix = GetWorldMatrix(object_2d_data);
+
+	//CObject2D::Draw(object_2d_data);
+	
+	//CObject2D::Draw(object_2d_data);
+	CObject2D::Draw(); //コミットビルドを通すための措置
+	//CObject2D::Draw(matrix,vertex_2d_,texture,renderstate);
+}
+
+//=============================================================================
+// 終了
+//=============================================================================
+void CRectangle2D::Uninit (void)
+{
+	SAFE_RELEASE(vertex_2d_);
+}
+
+//=============================================================================
+// 設定処理
+//=============================================================================
+void CRectangle2D::Set(void)
+{
+	switch(point_)
+	{
+		case POINT_CENTER:
+		{
+			//m_Position[0] = VECTOR2(-m_Size.x * 0.5f,-m_Size.y * 0.5f);
+			//m_Position[1] = VECTOR2(-m_Size.x * 0.5f, m_Size.y * 0.5f);
+			//m_Position[2] = VECTOR2( m_Size.x * 0.5f,-m_Size.y * 0.5f);
+			//m_Position[3] = VECTOR2( m_Size.x * 0.5f, m_Size.y * 0.5f);
+			position_[0] = VECTOR2(-size_._x * 0.5f,-size_._y * 0.5f);
+			position_[1] = VECTOR2(-size_._x * 0.5f,size_._y * 0.5f);
+			position_[2] = VECTOR2( size_._x * 0.5f,-size_._y * 0.5f);
+			position_[3] = VECTOR2( size_._x * 0.5f,size_._y * 0.5f);
+			break;
+		}
+		case POINT_LEFT_UP:
+		{
+			position_[0] = VECTOR2(    0.0f,     0.0f);
+			position_[1] = VECTOR2(    0.0f,-size_._y);
+			position_[2] = VECTOR2(size_._x,     0.0f);
+			position_[3] = VECTOR2(size_._x,-size_._y);
+			break;
+		}
+		case POINT_LEFT_MIDDLE:
+		{
+			position_[0] = VECTOR2(    0.0f, size_._y * 0.5f);
+			position_[1] = VECTOR2(    0.0f,-size_._y * 0.5f);
+			position_[2] = VECTOR2(size_._x, size_._y * 0.5f);
+			position_[3] = VECTOR2(size_._x,-size_._y * 0.5f);
+			break;
+		}
+		case POINT_LEFT_DOWN:
+		{
+			position_[0] = VECTOR2(    0.0f,size_._y);
+			position_[1] = VECTOR2(    0.0f,    0.0f);
+			position_[2] = VECTOR2(size_._x,size_._y);
+			position_[3] = VECTOR2(size_._x,    0.0f);
+			break;
+		}
+		case POINT_RIGHT_UP:
+		{
+			position_[0] = VECTOR2(-size_._x,     0.0f);
+			position_[1] = VECTOR2(-size_._x,-size_._y);
+			position_[2] = VECTOR2(     0.0f,     0.0f);
+			position_[3] = VECTOR2(     0.0f,-size_._y);
+			break;
+		}
+		case POINT_RIGHT_MIDDLE:
+		{
+			position_[0] = VECTOR2(-size_._x, size_._y * 0.5f);
+			position_[1] = VECTOR2(-size_._x,-size_._y * 0.5f);
+			position_[2] = VECTOR2(     0.0f, size_._y * 0.5f);
+			position_[3] = VECTOR2(     0.0f,-size_._y * 0.5f);
+			break;
+		}
+		case POINT_RIGHT_DOWN:
+		{
+			position_[0] = VECTOR2(-size_._x,size_._y);
+			position_[1] = VECTOR2(-size_._x,    0.0f);
+			position_[2] = VECTOR2(     0.0f,size_._y);
+			position_[3] = VECTOR2(     0.0f,    0.0f);
+			break;
+		}
+		case POINT_MIDDLE_UP:
+		{
+			position_[0] = VECTOR2(-size_._x * 0.5f,     0.0f);
+			position_[1] = VECTOR2(-size_._x * 0.5f,-size_._y);
+			position_[2] = VECTOR2( size_._x * 0.5f,     0.0f);
+			position_[3] = VECTOR2( size_._x * 0.5f,-size_._y);
+			break;
+		}
+		case POINT_MIDDLE_DOWN:
+		{
+			position_[0] = VECTOR2(-size_._x * 0.5f,size_._y);
+			position_[1] = VECTOR2(-size_._x * 0.5f,    0.0f);
+			position_[2] = VECTOR2( size_._x * 0.5f,size_._y);
+			position_[3] = VECTOR2( size_._x * 0.5f,    0.0f);
+			break;
+		}
+	}
+
+	CVertex2D::VERTEX_2D* pVertex2D = NULL;
+
+	vertex_2d_->Lock(&pVertex2D,NULL);
+
+	// 頂点座標の設定
+	pVertex2D->_position[0] = VECTOR2(position_[0]._x,position_[0]._y);
+	pVertex2D->_position[1] = VECTOR2(position_[2]._x,position_[2]._y);
+	pVertex2D->_position[2] = VECTOR2(position_[1]._x,position_[1]._y);
+	pVertex2D->_position[3] = VECTOR2(position_[3]._x,position_[3]._y);
+
+	// 法線情報の設定
+	//pVertex2D->_normal[0] = VECTOR2(0.0f,0.0f);
+	//pVertex2D->_normal[1] = VECTOR2(0.0f,0.0f);
+	//pVertex2D->_normal[2] = VECTOR2(0.0f,0.0f);
+	//pVertex2D->_normal[3] = VECTOR2(0.0f,0.0f);
+
+	// テクスチャ座標の設定
+	pVertex2D->_texcoord[0] = VECTOR2(left_,top_);
+	pVertex2D->_texcoord[1] = VECTOR2(right_,top_);
+	pVertex2D->_texcoord[2] = VECTOR2(left_,bottom_);
+	pVertex2D->_texcoord[3] = VECTOR2(right_,bottom_);
+
+	// 色情報の設定
+	pVertex2D->_color[0] = color_[0];
+	pVertex2D->_color[1] = color_[2];
+	pVertex2D->_color[2] = color_[3];
+	pVertex2D->_color[3] = color_[1];
+
+	// 頂点情報をアンロックする
+	vertex_2d_->Unlock();
+}
+
+
+#if 0
+//=============================================================================
+// 設定処理
+//=============================================================================
+void CRectangle2D::Set(void)
+{
+	switch(m_Point)
+	{
+		case POINT_CENTER:
+		{
+			m_Position[0] = VECTOR2(-m_Size.x * 0.5f,-m_Size.y * 0.5f);
+			m_Position[1] = VECTOR2(-m_Size.x * 0.5f, m_Size.y * 0.5f);
+			m_Position[2] = VECTOR2( m_Size.x * 0.5f,-m_Size.y * 0.5f);
+			m_Position[3] = VECTOR2( m_Size.x * 0.5f, m_Size.y * 0.5f);
+			break;
+		}
+		case POINT_LEFT_UP:
+		{
+			m_Position[0] = VECTOR2(    0.0f,    0.0f);
+			m_Position[1] = VECTOR2(    0.0f,m_Size.y);
+			m_Position[2] = VECTOR2(m_Size.x,    0.0f);
+			m_Position[3] = VECTOR2(m_Size.x,m_Size.y);
+			break;
+		}
+		case POINT_LEFT_MIDDLE:
+		{
+			m_Position[0] = VECTOR2(    0.0f,-m_Size.y * 0.5f);
+			m_Position[1] = VECTOR2(    0.0f, m_Size.y * 0.5f);
+			m_Position[2] = VECTOR2(m_Size.x,-m_Size.y * 0.5f);
+			m_Position[3] = VECTOR2(m_Size.x, m_Size.y * 0.5f);
+			break;
+		}
+		case POINT_LEFT_DOWN:
+		{
+			m_Position[0] = VECTOR2(    0.0f,-m_Size.y);
+			m_Position[1] = VECTOR2(    0.0f,     0.0f);
+			m_Position[2] = VECTOR2(m_Size.x,-m_Size.y);
+			m_Position[3] = VECTOR2(m_Size.x,     0.0f);
+			break;
+		}
+		case POINT_RIGHT_UP:
+		{
+			m_Position[0] = VECTOR2(-m_Size.x,    0.0f);
+			m_Position[1] = VECTOR2(-m_Size.x,m_Size.y);
+			m_Position[2] = VECTOR2(     0.0f,    0.0f);
+			m_Position[3] = VECTOR2(     0.0f,m_Size.y);
+			break;
+		}
+		case POINT_RIGHT_MIDDLE:
+		{
+			m_Position[0] = VECTOR2(-m_Size.x,-m_Size.y * 0.5f);
+			m_Position[1] = VECTOR2(-m_Size.x, m_Size.y * 0.5f);
+			m_Position[2] = VECTOR2(     0.0f,-m_Size.y * 0.5f);
+			m_Position[3] = VECTOR2(     0.0f, m_Size.y * 0.5f);
+			break;
+		}
+		case POINT_RIGHT_DOWN:
+		{
+			m_Position[0] = VECTOR2(-m_Size.x,-m_Size.y);
+			m_Position[1] = VECTOR2(-m_Size.x,     0.0f);
+			m_Position[2] = VECTOR2(     0.0f,-m_Size.y);
+			m_Position[3] = VECTOR2(     0.0f,     0.0f);
+			break;
+		}
+		case POINT_MIDDLE_UP:
+		{
+			m_Position[0] = VECTOR2(-m_Size.x * 0.5f,    0.0f);
+			m_Position[1] = VECTOR2(-m_Size.x * 0.5f,m_Size.y);
+			m_Position[2] = VECTOR2( m_Size.x * 0.5f,    0.0f);
+			m_Position[3] = VECTOR2( m_Size.x * 0.5f,m_Size.y);
+			break;
+		}
+		case POINT_MIDDLE_DOWN:
+		{
+			m_Position[0] = VECTOR2(-m_Size.x * 0.5f,-m_Size.y);
+			m_Position[1] = VECTOR2(-m_Size.x * 0.5f,     0.0f);
+			m_Position[2] = VECTOR2( m_Size.x * 0.5f,-m_Size.y);
+			m_Position[3] = VECTOR2( m_Size.x * 0.5f,     0.0f);
+			break;
+		}
+	}
+}
+#endif
+//---------------------------------- EOF --------------------------------------
---
+++ framework/sources/interface/graphic/vertex/vertex_2d.h
@@ -0,0 +1,131 @@
+//*****************************************************************************
+//
+// 3D頂点クラス
+//
+// Author		: Tomohiro Kouno
+//
+//*****************************************************************************
+
+//*****************************************************************************
+/**
+*
+*	2D頂点クラス
+*
+*	// 使い方
+*	Create関数でインスタンスの生成を行う
+*	Init関数で初期化
+*	Lock関数で頂点情報の格納開始し、頂点バッファのポインタを取得
+*	Unlock関数で頂点情報の格納終了(頂点バッファの格納をここでやるので、これを呼ばないと反映されない)
+*	Draw関数で描画
+*
+*/
+//*****************************************************************************
+
+//*****************************************************************************
+// 多重定義防止
+//*****************************************************************************
+#pragma once
+
+#ifndef _VERTEX_2D_H_
+#define _VERTEX_2D_H_
+
+//*****************************************************************************
+// インクルード
+//*****************************************************************************
+// basic
+#include "basic/basic.h"
+
+// common
+#include "common/image/color/color4f.h"
+#include "common/math/math.h"
+
+//*****************************************************************************
+// ライブラリのリンク
+//*****************************************************************************
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+
+//*****************************************************************************
+// 構造体定義
+//*****************************************************************************
+
+//*****************************************************************************
+// クラスの前方参照
+//*****************************************************************************
+class CDeviceHolder;
+
+//*****************************************************************************
+// クラス定義
+//*****************************************************************************
+class CVertex2D : public CBasic
+{
+public:
+	typedef struct _VERTEX_2D
+	{
+		VECTOR2* _position;
+		//VECTOR2* _normal;
+		COLOR4F* _color;
+		VECTOR2* _texcoord;
+	}VERTEX_2D;
+
+	typedef struct _VERTEX_2D_INDEX
+	{
+		u32* _position;
+		//u32* _normal;
+		u32* _color;
+		u32* _texcoord;
+	}VERTEX_2D_INDEX;
+
+	// 作成処理
+	static CVertex2D* Create(CDeviceHolder* device_holder,const u32& vertex_2d_number);
+
+	// 作成処理
+	static CVertex2D* Create(CDeviceHolder* device_holder,const u32& position_number,const u32& normal_number,const u32& color_number,const u32& texcoord_number);
+
+	// デストラクタ
+	virtual ~CVertex2D(void);
+
+	// 初期化処理
+	bool Init(void);
+
+	// 描画処理
+	//void Draw(const MATRIX4x4& matrix);
+
+	// 描画処理
+	//virtual void Draw(const MATRIX4x4& matrix,const u32& offset,const u32 length) = 0;
+
+	// 終了処理
+	virtual void Uninit(void);
+
+	// インデックスの作成
+	void CreateIndex(const int& index_number);
+
+	// ロック処理
+	void Lock(VERTEX_2D** vertex_2d,VERTEX_2D_INDEX** vertex_2d_index);
+
+	// 解除処理
+	virtual void Unlock(void) = 0;
+
+protected:
+	// コンストラクタ
+	CVertex2D(CDeviceHolder* device_holder,const u32& position_number,const u32& normal_number,const u32& color_number,const u32& texcoord_number);
+
+	CDeviceHolder* device_holder_;
+	VERTEX_2D vertex_2d_;
+	u32 position_number_;
+	u32 normal_number_;
+	u32 color_number_;
+	u32 texcoord_number_;
+	u32 primitive_number_;
+	u32 index_number_;
+	VERTEX_2D_INDEX vertex_2d_index_;
+	bool use_index_;
+private:
+
+};
+
+#endif	// _VERTEX_2D_H_
+
+//---------------------------------- EOF --------------------------------------
