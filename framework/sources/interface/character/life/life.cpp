8d169fc4324b8d408c0931724ddcdb691bd4a829
ooigawakazuma <yajuutiti@gmail.com>
2015-01-22 12:51:15 +0900

    一応ビルボードのライフもあげておきます。

--- framework/sources/interface/character/life/life.cpp
+++ framework/sources/interface/character/life/life.cpp
@@ -1,6 +1,6 @@
 //*****************************************************************************
 //
-// ユーザーナンバークラス
+// ライフクラス
 //
 // Author		: Kazuma Ooigawa
 //
@@ -10,7 +10,7 @@
 //*****************************************************************************
 // インクルード
 //*****************************************************************************
-#include "user_number.h"
+#include "life.h"
 //billboard
 #include "interface/graphic/object/object_3d/element/billboard.h"
 #include "interface/interface_manager.h"
@@ -26,22 +26,26 @@
 //=============================================================================
 // コンストラクタ
 //=============================================================================
-CUserNumber::CUserNumber(CInterfaceManager* interface_manager)
+CLife::CLife(CInterfaceManager* interface_manager , s32 max_life , s32 max_length )
 {
 	interface_manager_ = interface_manager;
+	max_life_ = max_life;
+	life_ = max_life;
+	max_length_ = max_length;
+	length_ = max_length;
 }
 
 //=============================================================================
 // デストラクタ
 //=============================================================================
-CUserNumber::~CUserNumber(void)
+CLife::~CLife(void)
 {
 }
 
 //=============================================================================
 // 生成処理
 //=============================================================================
-void CUserNumber::SetParameter(const VECTOR3& position , const s32& player_id )
+void CLife::SetParameter(const VECTOR3& position , const s32& player_id )
 {
 	position_ = position;
 	player_id_ = player_id;
@@ -50,43 +54,47 @@ void CUserNumber::SetParameter(const VECTOR3& position , const s32& player_id )
 //=============================================================================
 // 初期化処理
 //=============================================================================
-bool CUserNumber::Init(void)
+bool CLife::Init(void)
 {
 	// オブジェクトビルボードの生成
-	CBillboard* object_bill = new CBillboard(interface_manager_->graphic_manager()->device_holder());
-	object_bill->set_size(VECTOR2(10.0f,10.0f));
-	object_bill->Set();
+	object_bill_ = new CBillboard(interface_manager_->graphic_manager()->device_holder());
+	object_bill_->set_size(VECTOR2((f32)max_length_,10.0f));
+	object_bill_->set_color( COLOR4F( 0.0f , 1.0f , 0.0f , 1.0f ) );
+	object_bill_->Set();
 
 	// オブジェクトリストに追加
-	object_key_ = interface_manager_->graphic_manager()->object_manager()->object_3d_manager()->AddList(object_bill);
+	object_key_ = interface_manager_->graphic_manager()->object_manager()->object_3d_manager()->AddList(object_bill_);
 	return true;
 }
 
 //=============================================================================
 // 更新処理
 //=============================================================================
-void CUserNumber::Update(void)
+void CLife::Update(void)
 {
+	length_ = life_ / max_life_ * max_length_;
+	object_bill_->set_size( VECTOR2((f32)length_ , 10.0f ));
+	object_bill_->set_color( COLOR4F( 1.0f - life_ / max_life_ , life_ / max_life_ , 0.0f , 1.0f ) );
+	object_bill_->Set();
 }
 
 //=============================================================================
 // 描画処理
 //=============================================================================
-void CUserNumber::Draw(void)
+void CLife::Draw(void)
 {
 	CGraphicManager* graphic_manager = interface_manager_->graphic_manager();
 	CObjectManager* object_manager = graphic_manager->object_manager();
 	CObject3DManager* object_3d_manager = object_manager->object_3d_manager();
-	char* filename = NULL;
+
 	// 描画
-	sprintf( filename , "resources/texture/game _%d",0);
-	object_3d_manager->Draw(object_key_,position_,VECTOR3(),VECTOR3(1.0f,1.0f,1.0f),MATRIX4x4(),filename);
+	object_3d_manager->Draw(object_key_,position_,VECTOR3(),VECTOR3(1.0f,1.0f,1.0f),MATRIX4x4(),"snowball001");
 }
 
 //=============================================================================
 // 終了処理
 //=============================================================================
-void CUserNumber::Uninit( void )
+void CLife::Uninit( void )
 {
 	CGraphicManager* graphic_manager = interface_manager_->graphic_manager();
 	CObjectManager* object_manager = graphic_manager->object_manager();
@@ -98,7 +106,7 @@ void CUserNumber::Uninit( void )
 //=============================================================================
 // 消去処理
 //=============================================================================
-void CUserNumber::Erase( void )
+void CLife::Erase( void )
 {
 	is_death_ = true;
 }
